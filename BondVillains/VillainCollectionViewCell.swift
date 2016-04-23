3337f0511ced459283948489069236e68a0f1dcb
LakshmiEvani <Souji@Ravi-Evanis-MacBook-Pro.local>
2016-04-23 12:58:09 -0400

    added collection view

---
+++ BondVillains/VillainCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  VillainCollectionViewCell.swift
+//  BondVillains
+//
+//  Created by Souji on 4/23/16.
+//  Copyright © 2016 Souji. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class VillainCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var villainImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    //@IBOutlet weak var schemeLabel: UILabel!
+}
